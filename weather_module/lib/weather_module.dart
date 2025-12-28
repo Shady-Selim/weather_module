318766a7392df32da820973dbb16ed4cf8a4ecac
Shady Selim <shady.selim@hotmail.com>
2025-12-28 15:58:20 +0200

    Enhance weather module with error handling and data validation improvements

--- weather_module/lib/weather_module.dart
+++
@@ -1,8 +0,0 @@
-
-import 'weather_module_platform_interface.dart';
-
-class WeatherModule {
-  Future<String?> getPlatformVersion() {
-    return WeatherModulePlatform.instance.getPlatformVersion();
-  }
-}
